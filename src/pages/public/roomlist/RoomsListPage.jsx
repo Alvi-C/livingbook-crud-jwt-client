7f1cedbfebb934f4365098533f057fc62ab554c5
Alvi Chowdhury <alvic5050@gmail.com>
2023-11-06 12:15:43 +0600

    folder structure done

---
+++ src/pages/public/roomlist/RoomsListPage.jsx
@@ -0,0 +1,11 @@
+
+
+const RoomsListPage = () => {
+    return (
+        <div>
+            <h2>Room&aposs list</h2>
+        </div>
+    );
+};
+
+export default RoomsListPage;
---
+++ src/pages/public/signup/SignupPage.jsx
@@ -0,0 +1,10 @@
+
+const SignupPage = () => {
+    return (
+        <div>
+            <h2>Signup</h2>
+        </div>
+    );
+};
+
+export default SignupPage;
